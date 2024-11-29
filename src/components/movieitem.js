9afa51695057c7a4cc7486978be05c489c3242ad
CianBarrettATU <G00416157@atu.ie>
2024-11-29 14:44:28 +0000

    first commit

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,11 +1,20 @@
 import { useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import axios from "axios";
 
-const MovieItem = (props)=> {
-  useEffect(() => {
-    console.log("Movie Item:", props.mymovie);
-  }, [props.mymovie]); // Only run this effect when the mymovie prop changes
+function MovieItem(props) {
+  const handleDelete = (e) => {
+      e.preventDefault();
+      axios.delete('http://localhost:4000/api/movie/' + props.myMovie._id)
+          .then(() => {
+              props.Reload(); // Refresh the movie list after deletion
+          })
+          .catch((error) => {
+              console.error("Error deleting movie:", error);
+          });
+  }; // Only run this effect when the mymovie prop changes
 
   return (
     <div>
@@ -16,6 +25,7 @@ const MovieItem = (props)=> {
             <img src={props.mymovie.poster} alt={props.mymovie.title} />
             <footer>{props.mymovie.year}</footer>
           </blockquote>
+          <Button variant="danger" onClick={handleDelete}>Delete</Button>
         </Card.Body>
         <Link className="btn btn-primary" to={"/edit/"+props.mymovie._id}>Edit</Link>
       </Card>
